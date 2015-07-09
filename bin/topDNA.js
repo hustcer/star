3a4450f23b189f8f24794b650de973ace9f65f7a
hustcer <hustcer@gmail.com>
2015-07-09 17:35:19 +0800

    Add watch feature

--- bin/topDNA.js
+++ bin/topDNA.js
@@ -28,13 +28,32 @@ cmd
   .version('v0.0.1')
   .description('Private utils.')
   .option('-i, --insider'          , 'query insider tradings in batch mode.')
+  .option('-w, --watch'            , 'watch specified symbols.')
   .parse(process.argv);
 
 let Actions = {
-    'default' : function(){
+    'DEFAULT' : function(){
         console.log('Check the usage infomation for more details.');
     },
-    'insider' : function(){
+    'WATCH' : function(){
+        let Watch = require('../lib/watch.js').Watch;
+        let watchList = [
+            {name:'厦门信达', code:'000701'},
+            {name:'龙生股份', code:'002625'},
+            {name:'歌华有线', code:'600037'},
+            {name:'千方科技', code:'002373'},
+            {name:'中国西电', code:'601179'},
+            {name:'中航动力', code:'600893'},
+            {name:'锦龙股份', code:'000712'},
+            {name:'中国卫星', code:'600118'},
+            {name:'中航飞机', code:'000768'},
+            {name:'安信信托', code:'600816'},
+            {name:'恒生电子', code:'600570'},
+            {name:'华胜天成', code:'600410'},
+        ];
+        Watch.doWatch(_.pluck(watchList, 'code').join(','));
+    },
+    'INSIDER' : function(){
         let Insider   = require('../lib/insider.js').Insider;
         let queryList = [{name:'泰亚股份', code:'002517'},
                          {name:'中科曙光', code:'603019'},
@@ -86,8 +105,9 @@ let Actions = {
 }
 
 let doAction = function(){
-    let action = 'default';
-    if(cmd.insider){  action = 'insider'; }
+    let action = 'DEFAULT';
+    if(cmd.insider){  action = 'INSIDER'; }
+    if(cmd.watch)  {  action = 'WATCH';   }
     Actions[action]();
 };
 
